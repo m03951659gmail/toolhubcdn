38b0890d70b0c5eeee61ed995f36ad5d2a7c1795
m03951659gmail <m03951659@gmail.com>
2025-09-20 16:25:21 +0600

    Update allfileadd.js

--- allfileadd.js
+++ allfileadd.js
@@ -1,22 +1,11 @@
-// main.js
-function loadScript(url) {
-  return new Promise((resolve, reject) => {
-    const script = document.createElement("script");
-    script.src = url;
-    script.onload = resolve;
-    script.onerror = reject;
+function loadScript(src, callback) {
+    const script = document.createElement('script');
+    script.src = src;
+    script.onload = callback;
     document.head.appendChild(script);
-  });
 }
 
-// লোকাল ফাইল load
-loadScript("localfile1.js")
-  .then(() => loadScript("localfile2.js"))
-  .then(() => console.log("সব ফাইল লোড হয়ে গেছে ✅"))
-  .catch(err => console.error("লোডিং এ সমস্যা:", err));
-
-// CDN থেকে ফাইল load
-loadScript("https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js")
-  .then(() => {
-    console.log("Axios loaded from CDN ✅");
-  });
+// নতুন ফিচার লোড করুন
+loadScript('domainblocked.js', function() {
+    console.log("New feature loaded!");
+});
